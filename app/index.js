c69b442b3200fa2fdca80db637351d7321e3d71f
brewchetta <chett.tiller@gmail.com>
2021-01-05 16:29:50 -0500

    adding event listener, changed db.json to use properly cased artist

--- app/index.js
+++ app/index.js
@@ -5,13 +5,20 @@ const lyricsDisplay = document.querySelector('#lyrics-display')
 const pauseButton = document.querySelector('#song-controls-container').children[0]
 const cancelButton = document.querySelector('#song-controls-container').children[1]
 
+const startSong = song => {
+  songsList.style.display = "none"
+  lyricsDisplay.style.display = "block"
+  console.log(song);
+}
+
 const renderSongsList = songs => {
-  for (var song in songs) {
-    console.log(song)
+  songs.forEach(song => {
     const newButton = document.createElement('button')
     newButton.innerText = `${song.title} by ${song.artist}`
     songsList.append(newButton)
-  }
+    newButton.addEventListener("click", () => startSong(song))
+
+  })
 }
 
 const getSongs = () => {
@@ -22,3 +29,5 @@ const getSongs = () => {
     renderSongsList(data)
   })
 }
+
+getSongs()
