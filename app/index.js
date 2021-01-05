dc95e2f44d8f4c114ab82f000ed34201d9e987b9
brewchetta <chett.tiller@gmail.com>
2021-01-05 16:25:23 -0500

    songs up and running

--- app/index.js
+++ app/index.js
@@ -1,1 +1,24 @@
-// Your code goes here...
+let songs
+const URL = 'http://localhost:3000/songs'
+const songsList = document.querySelector('#songs-list')
+const lyricsDisplay = document.querySelector('#lyrics-display')
+const pauseButton = document.querySelector('#song-controls-container').children[0]
+const cancelButton = document.querySelector('#song-controls-container').children[1]
+
+const renderSongsList = songs => {
+  for (var song in songs) {
+    console.log(song)
+    const newButton = document.createElement('button')
+    newButton.innerText = `${song.title} by ${song.artist}`
+    songsList.append(newButton)
+  }
+}
+
+const getSongs = () => {
+  fetch(URL)
+  .then(res => res.json())
+  .then(data => {
+    songs = data
+    renderSongsList(data)
+  })
+}
